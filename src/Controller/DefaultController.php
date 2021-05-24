a066958565ba1f3629ba61c7dcfe2804d354a53b
Anne <ga.annegren@gmail.com>
2021-05-25 00:28:07 +0200

    sym04

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,7 +16,7 @@ class DefaultController extends AbstractController
 
 public function index(): Response
 {
-    return $this->render('default/index.html.twig', [
+    return $this->render('program/index.html.twig', [
        'website' => 'Wild Séries',
     ]);
 }
